7589bc2e3aae9ef5ca4364d0e764d37160321170
qa023 <qa.02@work.brins.co.id>
2026-09-12 18:15:09 +0700

    12/9/2026

---
+++ Object Repository/Data UI/Download/popupDownload.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>popupDownload</name>
+   <tag></tag>
+   <elementGuidId>50e4b940-e7a6-4370-91d0-7478efa3bfb6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//h5[@class='modal-title' and normalize-space()='File Download']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//h5[@class='modal-title' and normalize-space()='File Download']</value>
+      <webElementGuid>ccfd1656-9810-4d8d-8269-e46e419fe1a3</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Data UI/Filter FBI_Detail/Headline.rs
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Headline</name>
+   <tag></tag>
+   <elementGuidId>e0934227-7978-4a9b-b72f-fd4b4faa4667</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@class='tittle-text' and normalize-space(.)='Dashboard Performance BRINS-BRI FBI Detail']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='tittle-text' and normalize-space(.)='Dashboard Performance BRINS-BRI FBI Detail']</value>
+      <webElementGuid>835a1653-c78b-4345-9169-083260305d91</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
